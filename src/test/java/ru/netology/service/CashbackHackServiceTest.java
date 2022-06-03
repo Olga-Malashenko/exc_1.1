2cd1947a76cab4a08daa4298636fd712fdb1b287
Olga Malashenko <olga_birjukova@mail.ru>
2022-06-04 00:21:42 +0300

    file CashbackHackServiceTest.java were changed for fail

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,7 +11,7 @@ class CashbackHackServiceTest {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 3000;
         int actual = cashbackHackService.remain(amount);
-        int expected = 40;
+        int expected = 0;
         assertEquals (expected, actual);
 
     }
